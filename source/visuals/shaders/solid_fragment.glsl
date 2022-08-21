c8a0b38c32cd41e27f7301bcaf7f5a30db5c778a
Felix Kugler <relgukxilef@gmail.com>
2022-08-21 21:34:23 +0200

    Draw checkerboard

--- source/visuals/shaders/solid_fragment.glsl
+++ source/visuals/shaders/solid_fragment.glsl
@@ -3,6 +3,15 @@
 
 layout(location = 0) out vec4 color;
 
+layout(location = 0) in vec2 uv;
+
+float filtered_step(float x) {
+    float width = fwidth(x);
+    return clamp(x / width + 0.5, 0, 1);
+}
+
 void main() {
     color = vec4(0.11,0.839,0.808, 1.0);
+    color *=
+        vec4(filtered_step((fract(uv.x * 4) - 0.5) * (fract(uv.y * 4) - 0.5)));
 }
