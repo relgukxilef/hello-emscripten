570f1fc157f2fddee19ec001278db6f7a70c7412
Felix Kugler <relgukxilef@gmail.com>
2022-08-23 10:27:12 +0000

    Improve filtering of checkerboard

--- source/visuals/shaders/solid_fragment.glsl
+++ source/visuals/shaders/solid_fragment.glsl
@@ -5,13 +5,15 @@ layout(location = 0) out vec4 color;
 
 layout(location = 0) in vec2 uv;
 
-float filtered_step(float x) {
-    float width = fwidth(x);
-    return clamp(x / width + 0.5, 0, 1);
+vec2 filtered_step(vec2 x, vec2 width) {
+    return clamp(x / width + vec2(0.5), vec2(0), vec2(1));
 }
 
 void main() {
     color = vec4(0.11,0.839,0.808, 1.0);
-    color *=
-        vec4(filtered_step((fract(uv.x * 4) - 0.5) * (fract(uv.y * 4) - 0.5)));
+
+    vec2 p = uv * 4 + 0.25;
+    p = filtered_step(min(fract(p), 1 - fract(p)) - 0.25, fwidth(p));
+    
+    color *= mix(p.y, 1 - p.y, p.x);
 }
