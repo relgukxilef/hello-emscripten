56f87a939cdebd20bfbcf3de0e3fc0174edd7686
Felix Kugler <relgukxilef@gmail.com>
2022-10-24 02:22:53 +0200

    Echo positions to all open connections

--- source/server-main.cpp
+++ source/server-main.cpp
@@ -8,62 +8,48 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 
+#include "network/server.h"
+
 enum { port = 28750 };
 
-void notify_thread(unsigned thread) {
-
-}
-
-template<class T>
-struct read_locked {
-    ~read_locked() {
-        read_locks--;
-    }
-
-    T& t;
-    std::atomic_uint& read_locks;
-};
-
-template<class T>
-struct double_buffered {
-    std::optional<read_locked<T>> try_read();
-    T& write();
-    void swap_or_replace();
-
-    T buffers[2];
-    std::atomic_uint read_locks = 0;
-    std::atomic_bool write_buffer = 0;
-};
-
 struct session {
-    session(boost::asio::ip::tcp::socket&& socket) :
-        socket(std::move(socket)) {}
-    boost::asio::ip::tcp::socket socket;
+    session(boost::asio::ip::tcp::socket&& socket) : stream(std::move(socket)) {
+    }
     boost::beast::http::request<boost::beast::http::string_body> request;
-    std::optional<boost::beast::websocket::stream<boost::asio::ip::tcp::socket>>
-        stream;
+    boost::beast::websocket::stream<boost::asio::ip::tcp::socket> stream;
     boost::beast::flat_buffer buffer;
     // TODO: may need a queue for messages because async_write cannot be called
     // before the last async_write completed
 };
 
-void read(session& session) {
-    session.stream->async_read(session.buffer, [&session](
-        boost::beast::error_code error,
-        size_t size
+std::vector<glm::vec3>* positions;
+std::vector<session>* sessions;
+
+void read(unsigned session_index, unsigned user_index);
+
+void write(unsigned session_index, unsigned user_index);
+
+void read(unsigned session_index, unsigned user_index) {
+    auto& session = (*sessions)[session_index];
+    session.stream.async_read(session.buffer, [session_index, user_index](
+        boost::beast::error_code error, size_t size
     ) {
+        auto& session = (*sessions)[session_index];
         printf(
-            "T%s WS Read %s.\n",
-            boost::lexical_cast<std::string>(
-                std::this_thread::get_id()
-            ).c_str(),
+            "U%d WS Read %s.\n",
+            user_index,
             boost::beast::buffers_to_string(session.buffer.cdata()).c_str()
         );
         if (!error) {
             // TODO
+            if (size >= sizeof(glm::vec3)) {
+                (*positions)[user_index] = *reinterpret_cast<const glm::vec3*>(
+                    session.buffer.cdata().data()
+                );
+            }
 
             session.buffer.consume(session.buffer.size());
-            read(session);
+            write(session_index, user_index);
         } else {
             // TODO: sessions are never deleted
             printf("Error %s.\n", error.message().c_str());
@@ -71,10 +57,29 @@ void read(session& session) {
     });
 }
 
+void write(unsigned session_index, unsigned user_index) {
+    auto& session = (*sessions)[session_index];
+
+    session.stream.async_write(
+        boost::asio::buffer(*positions), [session_index, user_index](
+            boost::beast::error_code error, size_t size
+        ) {
+            auto& session = (*sessions)[session_index];
+            printf("U%d WS Write.\n", user_index);
+            if (!error) {
+                read(session_index, user_index);
+            } else {
+                // TODO: sessions are never deleted
+                printf("Error %s.\n", error.message().c_str());
+            }
+        }
+    );
+}
+
 void accept(
-    boost::asio::ip::tcp::acceptor& acceptor, std::vector<session>& sessions
+    boost::asio::ip::tcp::acceptor& acceptor
 ) {
-    acceptor.async_accept(acceptor.get_executor(), [&acceptor, &sessions](
+    acceptor.async_accept(acceptor.get_executor(), [&acceptor](
         boost::system::error_code error,
         boost::asio::ip::tcp::socket&& socket
     ) {
@@ -89,13 +94,15 @@ void accept(
         );
 
         if (!error) {
-            sessions.push_back(session{std::move(socket)});
-            auto& session = sessions.back();
+            unsigned session_index = sessions->size();
+            sessions->push_back(std::move(socket));
+            auto& session = sessions->back();
             boost::beast::http::async_read(
-                session.socket, session.buffer, session.request,
-                [&session](
+                session.stream.next_layer(), session.buffer, session.request,
+                [session_index](
                     boost::beast::error_code error, std::size_t size
                 ) {
+                    auto& session = (*sessions)[session_index];
                     printf(
                         "T%s HTTP Read %s.\n",
                         boost::lexical_cast<std::string>(
@@ -109,14 +116,14 @@ void accept(
                         if (
                             boost::beast::websocket::is_upgrade(session.request)
                         ) {
-                            session.stream.emplace(
-                                std::move(session.socket)
-                            );
-                            session.stream->async_accept(
+                            auto user_index = positions->size();
+                            positions->push_back({});
+                            session.stream.async_accept(
                                 session.request,
-                                [&session](
+                                [session_index, user_index](
                                     boost::beast::error_code error
                                 ) {
+                                    auto& session = (*sessions)[session_index];
                                     printf(
                                         "T%s WS Accept.\n",
                                         boost::lexical_cast<std::string>(
@@ -124,7 +131,7 @@ void accept(
                                         ).c_str()
                                     );
                                     if (!error) {
-                                        read(session);
+                                        read(session_index, user_index);
                                     } else {
                                         printf(
                                             "Error %s.\n",
@@ -140,7 +147,7 @@ void accept(
                     } else if(
                         error == boost::beast::http::error::end_of_stream
                     ) {
-                        session.socket.shutdown(
+                        session.stream.next_layer().shutdown(
                             boost::asio::ip::tcp::socket::shutdown_send, error
                         );
                     } else {
@@ -149,7 +156,7 @@ void accept(
                 }
             );
 
-            accept(acceptor, sessions);
+            accept(acceptor);
         }
     });
 }
@@ -157,7 +164,10 @@ void accept(
 int main() {
     boost::asio::io_context context;
 
+    std::vector<glm::vec3> positions;
+    ::positions = &positions;
     std::vector<session> sessions;
+    ::sessions = &sessions;
 
     boost::asio::ip::tcp::acceptor acceptor(
         context, boost::asio::ip::tcp::endpoint{{}, port}
@@ -169,7 +179,7 @@ int main() {
 
     acceptor.listen();
 
-    accept(acceptor, sessions);
+    accept(acceptor);
 
     // signal_set does not work on Windows
 
